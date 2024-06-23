3c2e8762ed663412aafff294f8f605e75ef5697a
sirawan1003 <sirawan06102546@gmail.com>
2024-06-23 13:06:40 +0700

    change picture

--- src/app/_components/Profile.tsx
+++ src/app/_components/Profile.tsx
@@ -8,9 +8,9 @@ const Profile = (props: Props) => {
     <div className="bg-blue-100 h-full p-4 rounded-xl px-4">
       <div className="flex justify-center items-center pt-4 pb-2">
         <Image
-          src="/getstudentimage.jpg"
-          width={600}
-          height={700}
+          src="/ponyo034.jpg"
+          width={500}
+          height={500}
           alt="profile picture"
           className="w-3/4 aspect-square rounded-full border-4 border-white"
         />
